0c517f0527c3d24b40000b68f6a404039974d193
Chaithanya013 <chaithanyav.0203@gmail.com>
2026-01-02 15:23:03 +0530

    Add PostgreSQL integration and DB migrations

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,32 @@
 import os
-from flask import Flask
+import psycopg2
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        connect_timeout=5
+    )
+
+def run_migrations():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        with open("migrations/001_create_users.sql", "r") as f:
+            cur.execute(f.read())
+
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("Database migrations completed")
+    except Exception as e:
+        print("Migration error:", e)
 
 @app.route("/")
 def home():
@@ -9,12 +34,21 @@ def home():
 
 @app.route("/health")
 def health():
-    return {
-        "status": "UP",
-        "service": "backend"
-    }, 200
-
+    try:
+        conn = get_db_connection()
+        conn.close()
+        return jsonify({
+            "status": "UP",
+            "db": "CONNECTED"
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "status": "DOWN",
+            "db": "NOT_CONNECTED",
+            "error": str(e)
+        }), 500
 
 if __name__ == "__main__":
+    run_migrations()
     port = int(os.getenv("BACKEND_PORT", 5000))
     app.run(host="0.0.0.0", port=port)
